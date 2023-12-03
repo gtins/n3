387c23aa45115f88651865eff4e1f9a6d89e5525
Gustavo <gu.martins@catolicasc.edu.br>
2023-12-03 18:07:39 -0300

    cliente com pedido e logica de negocio implementada

--- controllers/pedido_controller.js
+++ controllers/pedido_controller.js
@@ -11,14 +11,33 @@ export const getPedido = async (req, res) => {
 
 export const createPedido = async (req, res) => {
     try {
-        await pedido.create(req.body);
-        res.json({
-            "message":"Um novo registro de pedidos foi inserido!"
-        })
+        const { cod_produto, qtde_pedido } = req.body;
+
+        let novaQtdePedido = 0;
+
+        if (qtde_pedido <= 3) {
+            novaQtdePedido = 4;
+        } else if (qtde_pedido > 3 && qtde_pedido < 7) {
+            novaQtdePedido = 3;
+        } else {
+            return res.status(400).json({ message: 'Quantidade do produto não atende aos critérios para criação do pedido.' });
+        }
+
+        const novoPedido = await pedido.create({
+            cod_produto,
+            qtde_pedido: novaQtdePedido
+        });
+
+        res.status(201).json({
+            message: 'Um novo registro de pedido foi inserido!',
+            pedido: novoPedido
+        });
     } catch (e) {
-        console.log("Erro ao inserir um novo registro",e);
+        console.log('Erro ao inserir um novo registro de pedido', e);
+        res.status(500).json({ error: 'Erro ao inserir um novo registro de pedido' });
     }
-}
+};
+
 
 export const updatePedido = async (req, res) => {
     try {
