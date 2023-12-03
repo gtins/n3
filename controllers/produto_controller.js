cf1d1ff2f609e9b006fd7b21430d0ade4063602d
Gustavo <gu.martins@catolicasc.edu.br>
2023-12-03 16:41:24 -0300

    produto controller e route

--- controllers/produto_controller.js
+++ controllers/produto_controller.js
@@ -1,1 +1,58 @@
 import produto from "../models/produto_model.js" //vamos para o model!
+
+export const getProdutos = async (req, res) => {
+    try {
+        const produtos = await produto.findAll()
+        res.send(produtos)
+    } catch (e) {
+        console.log("Erro ao acessar a tabela Produtos",e)
+    }
+} 
+
+export const createProdutos = async (req, res) => {
+    try {
+        await produto.create(req.body)
+        res.json({
+            "message":"Um novo registro de produtos foi inserido"
+        })
+
+    } catch (e) {
+        console.log("Erro ao inserir um novo registro", e)
+
+    }
+}
+
+export const updateProduto = async (req, res) => {
+    try {
+        await produto.update(req.body,{
+            where: {
+                cod_produto: req.params.cod_r
+            }
+        })
+        res.json({
+            "message":"O produto " + req.params.cod_r +  " foi atualizado"
+        })
+    } catch (e) {
+        console.log("Erro ao atualizar registro Produto!")
+
+    }
+}
+
+export const deleteProduto = async (req, res) => {
+    try {
+        await produto.destroy({
+            where: {
+                cod_produto: req.params.cod_r
+            }
+        })
+        res.json({
+            "message":"O produto " + req.params.cod_r +  " foi excluído"
+        })
+    } catch (e) {
+        console.log("Erro ao excluir registro Produto!")
+
+    }
+}
+
+
+
--- routes/produto_routes.js
+++ routes/produto_routes.js
@@ -1,2 +1,11 @@
 import express from 'express' //beleza 
-import { } from '../controllers/produto_controller.js' //vamos para o controller!
+import { getProdutos, createProdutos, updateProduto, deleteProduto } from '../controllers/professor_controller.js' //vamos para o controller!
+
+const router = express.Router()
+
+router.get('/produto', getProdutos)
+router.post('/produto', createProdutos)
+router.put('/produto/:cod_r', updateProduto)
+router.delete('/produto/:cod_r', deleteProduto)
+
+export default router
